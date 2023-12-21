2df3d3e401e07399a590425602da846a32b57d60
kenichi beveridge <kenbev84@gmail.com>
2023-12-21 16:22:53 +0000

    added Tests and code for User Model

---
+++ backend/tests/unit/user.model.test.js
@@ -0,0 +1,32 @@
+import { expect } from 'chai';
+
+import User from "../../src/models/user.model.js";
+
+
+describe('User Model Tests', () => {
+    it('should create an instance of User', (done) => {
+
+        // Arrange
+        const newUser = {
+            firstName: "Dave",
+            lastName: "Test",
+            userName: "testyDave",
+            email: "dave@test.com",
+            password: "password"
+
+        }
+
+        // Act
+        const user = new User(newUser);
+
+        console.log(user);
+
+        expect(user).to.have.property("firstName").to.equal(newUser.firstName);
+        expect(user).to.have.property("lastName").to.equal(newUser.lastName);
+        expect(user).to.have.property("userName").to.equal(newUser.userName);
+        expect(user).to.have.property("email").to.equal(newUser.email);
+        expect(user).to.have.property("_id").to.a('object')
+        done()
+
+    })
+})
